227dc2d1942742fe11746fe82d15e5cb412689aa
ahmed waly <ahmedwaly074@gmail.com>
2026-02-08 19:41:14 +0200

    update CRUD added GET Date and SET Date

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ let productDescriptionInput = document.getElementById("productDescription");
 let btn = document.getElementById("btn");
 
 
+// Get localStorage
+if (localStorage.getItem("productlist") != null) {
+    productList = JSON.parse(localStorage.getItem("productlist"));
+    disPro();
+}
+// console.log(productList);
 // console.log(productNameInput);
 // console.log(productPriceInput);
 // console.log(productCategoryInput);
@@ -26,5 +32,32 @@ function addProduct() {
     }
     productList.push(pro);
     // console.log(pro);
+    disPro();
     console.log(productList);
+
+    //set localStorage 
+    localStorage.setItem("productlist", JSON.stringify(productList));
+
+}
+
+function disPro() {
+    var allProducts = ``;
+    for (let i = 0; i < productList.length; i++) {
+        allProducts += `
+               <tr>
+                 <td>${i + 1}</td>
+                 <td>${productList[i].name}</td>
+                 <td>${productList[i].price}</td>
+                 <td>${productList[i].category}</td>
+                 <td>${productList[i].description}</td>
+                 <td class="d-flex py-3">
+                     <button class="btn btn-outline-primary me-1">Update</button>
+                     <button class="btn btn-outline-danger ">Delete</button>
+                 </td>
+                </tr>
+
+        `
+    }
+    console.log(allProducts);
+    document.getElementById("tbody").innerHTML = allProducts;
 }
